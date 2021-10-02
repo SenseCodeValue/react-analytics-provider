28682f1b1880c2bb832051f87fa00e31048ff6f7
정대윤 <sensecodevalue@gmail.com>
2021-10-02 17:08:28 +0900

    feat: Add emotion package

--- demo/with-cra/src/App.tsx
+++ demo/with-cra/src/App.tsx
@@ -5,10 +5,12 @@ import LoginPage from './pages/LoginPage';
 import CurrencyPage from './pages/CurrencyPage';
 import UserPropertyPage from './pages/UserPropertyPage';
 import Route from './router/Route';
+import {Global, css} from '@emotion/react';
 
 function App() {
   return (
     <div className="App">
+      <Global styles={globalStyle}></Global>
       <NavBar />
       <Route path="/">
         <MainPage />
@@ -30,3 +32,25 @@ function App() {
 }
 
 export default App;
+
+const globalStyle = css`
+  body {
+    background: #f5f5f5;
+    color: #4d4d4d;
+    font-size: 14px;
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+  }
+  button {
+    padding: 0;
+    font: inherit;
+    color: inherit;
+    border: 0;
+    border-radius: 0;
+    background: none;
+    appearance: none;
+    box-shadow: none;
+    overflow: visible;
+    cursor: pointer;
+  }
+`;
---
+++ demo/with-cra/src/components/Button.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const Button = styled('button')`
+  padding: 20px;
+  background-color: #eee;
+  border-radius: 3px;
+`;
+
+export default Button;
